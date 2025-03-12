7f999cc0da487663a1298b93fc6882bf9c382ab9
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-03-12 16:03:10 +0600

    Added DbSets

--- src/Core/BlazorChat.Core.Infrastructure/DataAccess/ApplicationDbContext.cs
+++ src/Core/BlazorChat.Core.Infrastructure/DataAccess/ApplicationDbContext.cs
@@ -6,5 +6,8 @@ namespace BlazorChat.Core.Infrastructure.DataAccess;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): IdentityDbContext<User>(options)
 {
-    
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserProfile> UserProfiles { get; set; }
 }
