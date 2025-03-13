c849d7e6d259df7d687a98153927ad1a6d86758b
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-03-13 11:05:45 +0600

    Updated Entities

--- src/Core/BlazorChat.Core.Domain/Entities/Group.cs
+++ src/Core/BlazorChat.Core.Domain/Entities/Group.cs
@@ -3,6 +3,8 @@
 public class Group
 {
     public int Id { get; set; }
+    public string Name { get; set; }
+    public string Icon { get; set; }
     public List<User> Members { get; set; } = new List<User>();
     public List<Message> Messages { get; set; } = new List<Message>();
 }
--- src/Core/BlazorChat.Core.Domain/Entities/Message.cs
+++ src/Core/BlazorChat.Core.Domain/Entities/Message.cs
@@ -5,6 +5,6 @@ public class Message
     public int Id { get; set; }
     public string Content { get; set; } = string.Empty;
     public DateTime Date { get; set; }
-    public int SenderId { get; set; }
-    public int ReceiverId { get; set; }
+    public int GroupId { get; set; }
+    public Group Group { get; set; }
 }
