6d6587105bf5466e06d6b7dd36ced61f33ecf51e
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-03-14 21:47:00 +0600

    Added ChatHub.cs

---
+++ src/Core/BlazorChat.Core.Application/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using BlazorChat.Core.Application.Dtos;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChat.Core.Application.Hubs;
+
+public class ChatHub: Hub
+{
+    public async Task SendMessage(SendMessageDto message)
+    {
+        if (message.ChatId == null && message.GroupId == null)
+            throw new ArgumentException("Сообщение должно принадлежать либо чату, либо группе.");
+
+        string groupName = message.ChatId != null ? $"chat_{message.ChatId}" : $"group_{message.GroupId}";
+
+        await Clients.Group(groupName).SendAsync("ReceiveMessage", message.SenderId, message.Content);
+    }
+
+    public async Task JoinChat(int chatId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, $"chat_{chatId}");
+    }
+
+    public async Task JoinGroup(int groupId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, $"group_{groupId}");
+    }
+}
