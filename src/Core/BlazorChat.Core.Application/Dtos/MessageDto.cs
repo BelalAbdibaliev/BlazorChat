7be8e2834bd43d07fc987bbd0a3e3e957337afbd
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-03-14 21:46:15 +0600

    Added dtos

---
+++ src/Core/BlazorChat.Core.Application/Dtos/MessageDto.cs
@@ -0,0 +1,20 @@
+﻿using BlazorChat.Core.Domain.Entities;
+
+namespace BlazorChat.Core.Application.Dtos;
+
+public class MessageDto
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    
+
+    public string SenderId { get; set; }
+    public User Sender { get; set; }
+
+    public int? GroupId { get; set; }
+    public Group? Group { get; set; }
+
+    public int? ChatId { get; set; }
+    public Chat? Chat { get; set; }
+}
---
+++ src/Core/BlazorChat.Core.Application/Dtos/SendMessageDto.cs
@@ -0,0 +1,17 @@
+﻿using BlazorChat.Core.Domain.Entities;
+
+namespace BlazorChat.Core.Application.Dtos;
+
+public class SendMessageDto
+{
+    public string Content { get; set; } = string.Empty;
+    
+    public string SenderId { get; set; }
+    public User Sender { get; set; }
+
+    public int? GroupId { get; set; }
+    public Group? Group { get; set; }
+
+    public int? ChatId { get; set; }
+    public Chat? Chat { get; set; }
+}
